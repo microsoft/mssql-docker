0e4dbd994dadf3a4fd292f8d65c962cd770d9f86
Luis Bosquez <luisdbosquez@gmail.com>
2017-02-27 16:51:10 -0800

    Python env dockerfile

---
+++ developer-resources/sample.py
@@ -0,0 +1,36 @@
+import pyodbc
+server = '$DB_HOST'
+username = '$DB_USERNAME'
+password = '$DB_PASSWORD'
+cnxn = pyodbc.connect('DRIVER={ODBC Driver 13 for SQL Server};SERVER='+server+';PORT=1443;DATABASE='+database+';UID='+username+';PWD='+ password)
+cursor = cnxn.cursor()
+
+print ('Inserting a new row into table')
+#Insert Query
+tsql = "INSERT INTO Employees (Name, Location) VALUES (?,?);"
+with cursor.execute(tsql,'Jake','United States'):
+    print ('Successfuly Inserted!')
+
+
+#Update Query
+print ('Updating Location for Nikita')
+tsql = "UPDATE Employees SET Location = ? WHERE Name = ?"
+with cursor.execute(tsql,'Sweden','Nikita'):
+    print ('Successfuly Updated!')
+
+
+#Delete Query
+print ('Deleting user Jared')
+tsql = "DELETE FROM Employees WHERE Name = ?"
+with cursor.execute(tsql,'Jared'):
+    print ('Successfuly Deleted!')
+
+
+#Select Query
+print ('Reading data from table')
+tsql = "SELECT Name, Location FROM Employees;"
+with cursor.execute(tsql):
+    row = cursor.fetchone()
+    while row:
+        print (str(row[0]) + " " + str(row[1]))
+        row = cursor.fetchone()
