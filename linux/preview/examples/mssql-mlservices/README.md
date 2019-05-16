512b5d974da7094cdda28e6e415cd033ba24f8af
trtran <trtran@microsoft.com>
2019-05-15 18:55:07 -0700

    update README.md and run.sh

--- linux/preview/examples/mssql-mlservices/README.md
+++ linux/preview/examples/mssql-mlservices/README.md
@@ -12,5 +12,32 @@
 2. Run the script `./build.sh` to build the image. By default, both R and Python Services are installed into the image. If you just want to install R Services, edit `./overlay/tmp/install.sh` and set MLSERVICES_PKGS="mssql-mlservices-packages-r" in the script. If you just want to install Python Services, set MLSERVICES_PKGS="mssql-mlservices-packages-py".
 3. Edit `run.sh` script to set variables MSSQL_PID, ACCEPT_EULA, ACCEPT_EULA_ML and PATH_TO_MSSQL. Make sure the folder specified by PATH_TO_MSSQL does exist. If not, create one.
 4. Run the script `./run.sh` to start the container.
-5. Connect to Linux SQL Server in the container and enable the external script.
-6. Run R/Python script using sp_execute_external_script.
+5. Connect to Linux SQL Server in the container and enable the external script by running the following TSQL statement:
+```
+EXEC sp_configure  'external scripts enabled', 1
+RECONFIGURE WITH OVERRIDE
+```
+6. Verify ML Services working by running the following simple R/Python sp_execute_external_script:
+```
+execute sp_execute_external_script 
+@language = N'R',
+@script = N'
+print("Hello World!")
+print(R.version)
+print(Revo.version)
+OutputDataSet <- InputDataSet', 
+@input_data_1 = N'select 1'
+go
+```
+
+```
+execute sp_execute_external_script 
+@language = N'Python',
+@script = N'
+import sys
+print(sys.version)
+print("Hello World!")
+OutputDataSet = InputDataSet',
+@input_data_1 = N'select 1'
+go 
+```
--- linux/preview/examples/mssql-mlservices/run.sh
+++ linux/preview/examples/mssql-mlservices/run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # This sample script sets environment variables and starts the docker container.
 
+#####################################
+# Set all variables for 'docker run '
+#####################################
+
 # set MSSQL_PID to specify SQL Server edition to use
 # Details about editions can be found at https://go.microsoft.com/fwlink/?LinkId=852748&clcid=0x409
 # E.g., To use the Developer edition: MSSQL_PID='Developer'
 #
-if [ -z "$MSSQL_PID" ]; then
-    echo "Error: Please edit this script to set MSSQL_PID to specify SQL Server edition to use."
-    exit 1
-fi
 
 # set ACCEPT_EULA to specify if you accept the EULA
 # set ACCEPT_EULA_ML to specify if you accept the Machine Learning EULA
@@ -18,6 +18,20 @@ fi
 # E.g., to accept the EULA: ACCEPT_EULA='Y'
 # E.g., to accept the EULA ML: ACCEPT_EULA_ML='Y'
 #
+
+# Path to mssql host dir which will be mapped to /var/opt/mssql
+# E.g., PATH_TO_MSSQL="${HOME}/mssql"
+#
+
+#####################################
+# Verify variables are all set
+#####################################
+
+if [ -z "$MSSQL_PID" ]; then
+    echo "Error: Please edit this script to set MSSQL_PID to specify SQL Server edition to use."
+    exit 1
+fi
+
 if [ -z "$ACCEPT_EULA" ]; then
     echo "Error: Please edit this script to set ACCEPT_EULA to specify whether or not you accept the EULA."
     exit 1
@@ -28,23 +42,26 @@ if [ -z "$ACCEPT_EULA_ML" ]; then
     exit 1
 fi
 
-# SA_PASSWORD to set the SQL Server system administrator password.
-#
-echo -n "Create a password for the SQL Server system administrator (SA account): "
-read -s SA_PASSWORD
-
-# Path to mssql host dir which will be mapped to /var/opt/mssql
-#
-PATH_TO_MSSQL="${HOME}/mssql"
 if [ ! -d "${PATH_TO_MSSQL}" ]; then
     echo "Error: ${PATH_TO_MSSQL} does not exist. Please create the directory or edit this script to set PATH_TO_MSSQL to an existing directory."
     exit 1
 fi
 
+#####################################
+# Read SA password from input
+#####################################
+# SA_PASSWORD to set the SQL Server system administrator password.
+#
+echo -n "Create a password for the SQL Server system administrator (SA account): "
+read -s SA_PASSWORD
+
+#####################################
+# Start 'docker run'
+#####################################
 # Docker image.
 #
 MSSQL_ML_DOCKER_IMG='mssql-server-mlservices:latest'
 
 # Start
-DOCKER_RUN="docker run -e MSSQL_PID=${MSSQL_PID} -e ACCEPT_EULA=${ACCEPT_EULA} -e ACCEPT_EULA_ML=${ACCEPT_EULA_ML} -e SA_PASSWORD=${SA_PASSWORD} -v ${PATH_TO_MSSQL}:/var/opt/mssql -p 1433:1433 ${MSSQL_ML_DOCKER_IMG}"
+DOCKER_RUN="docker run -d -e MSSQL_PID=${MSSQL_PID} -e ACCEPT_EULA=${ACCEPT_EULA} -e ACCEPT_EULA_ML=${ACCEPT_EULA_ML} -e SA_PASSWORD=${SA_PASSWORD} -v ${PATH_TO_MSSQL}:/var/opt/mssql -p 1433:1433 ${MSSQL_ML_DOCKER_IMG}"
 eval $DOCKER_RUN
